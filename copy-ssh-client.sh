bb2b8aa4210b82dd0fca65d30ae8348fe13f6d7b
Ricarte Venerayan <venerayan@gmail.com>
2018-03-10 22:03:58 -0500

    Update and rename create-inventory2.sh to copy-ssh-client.sh

---
+++ copy-ssh-client.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#make sure you are using user with sudo no password
+#please create a file with your password like this "echo Your_password > secret"
+
+if [ ! -f secret ]; then
+   echo "Please create your secret first! (echo Your_password > secret)"
+   exit 1
+fi
+
+#create the repo & install extra repo
+if [ ! -f /etc/yum.repos.d/centos.repo ]; then
+    sudo sh -c 'echo -e "[centos]\nname=CentOS $releasever - $basearch\nbaseurl=http://mirror.centos.org/centos/7/os/\$basearch/\nenabled=1\ngpgcheck=1\ngpgkey=http://mirror.centos.org/centos/7/os/\$basearch/RPM-GPG-KEY-CentOS-7" > /etc/yum.repos.d/centos.repo'
+    rpm -qa epel-release > x
+     if [[ ! -s x ]]; then
+        sudo wget https://archive.fedoraproject.org/pub/epel/7Server/x86_64/Packages/e/epel-release-7-11.noarch.rpm
+        sudo rpm -ivh epel-release-7-11.noarch.rpm
+     fi
+     #install sshpass git ansible
+     sudo yum install sshpass -y
+fi
+
+#prepare to connect to each server so that ansible can penetrate the remote server
+#prepare the template script sshcopy
+#if you are using password with sudo, use below instead and add the password,
+awk -F',' 'FNR > 1 { print "sshpass -p $(cat secret) ssh-copy-id -o StrictHostKeyChecking=no " $1 }' input.csv > sshcopy
+#change it to executable
+chmod u+x sshcopy
+#run it
+./sshcopy
