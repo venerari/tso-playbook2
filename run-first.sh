b8ef97525364d785f9075113b7398975a757a6e7
Ricarte Venerayan <venerayan@gmail.com>
2018-03-11 16:07:48 -0400

    Create run-first.sh

---
+++ run-first.sh
@@ -0,0 +1,10 @@
+#create the repo & install extra repo
+if [ ! -f /etc/yum.repos.d/centos.repo ]; then
+    sudo sh -c 'echo -e "[centos]\nname=CentOS $releasever - $basearch\nbaseurl=http://mirror.centos.org/centos/7/os/\$basearch/\nenabled=1\ngpgcheck=1\ngpgkey=http://mirror.centos.org/centos/7/os/\$basearch/RPM-GPG-KEY-CentOS-7" > /etc/yum.repos.d/centos.repo'
+    rpm -qa epel-release > x
+     if [[ ! -s x ]]; then
+        sudo wget https://archive.fedoraproject.org/pub/epel/7Server/x86_64/Packages/e/epel-release-7-11.noarch.rpm
+        sudo rpm -ivh epel-release-7-11.noarch.rpm
+        sudo yum install git ansible -y
+     fi
+fi
