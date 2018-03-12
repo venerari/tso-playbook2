f5714c625c999dc85fe992c170aff8ada9dcaf2f
Ricarte Venerayan <venerayan@gmail.com>
2018-03-11 20:56:31 -0400

    Update run-first.sh

--- run-first.sh
+++ run-first.sh
@@ -5,6 +5,6 @@ if [ ! -f /etc/yum.repos.d/centos.repo ]; then
      if [[ ! -s x ]]; then
         sudo wget https://archive.fedoraproject.org/pub/epel/7Server/x86_64/Packages/e/epel-release-7-11.noarch.rpm
         sudo rpm -ivh epel-release-7-11.noarch.rpm
-        sudo yum install git ansible -y
+        sudo yum install git ansible sshpass -y
      fi
 fi
