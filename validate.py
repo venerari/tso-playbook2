cf8802a05920629bbfb9b886d7ee71d771a58ace
Ricarte Venerayan <venerayan@gmail.com>
2018-03-12 16:52:44 -0400

    Update validate.py

--- validate.py
+++ validate.py
@@ -58,7 +58,6 @@ if __name__ == '__main__':
                 host_input = row_input['Hostname']
                 if host_input:
                     fact_file = host_input + '_output.csv'
-                    print('Processing fact file ' + fact_file )
                     msg = ""
                     if os.path.isfile(fact_file):
                         ipaddress_input = row_input['IPAddress']
@@ -73,6 +72,8 @@ if __name__ == '__main__':
                             for row_fact in reader_factfile:
                                 #_init_vars_fact()
                                 host_fact = ""
+                                checkhost_fact = ""
+                                port_fact = ""
                                 ipaddress_fact = ""
                                 cpucore_fact = ""
                                 ram_fact = ""
@@ -80,6 +81,8 @@ if __name__ == '__main__':
                                 msg = ""
 
                                 host_fact = row_fact['Hostname']
+                                checkhost_fact = row_fact['Check-Host']
+                                port_fact = row_fact['Port']
                                 ipaddress_fact = row_fact['IPAddress']
                                 cpucore_fact = row_fact['CPUCore']
                                 ram_fact = row_fact['RAM']
@@ -107,19 +110,18 @@ if __name__ == '__main__':
                                     fact_file = ""
                                     msg = ""
 
-                                output_dict = {'Hostname input': host_input, 'Hostname output': host_fact, 'IP input': ipaddress_input, 'IP fact': ipaddress_fact, 'CPU core input': cpucore_input, 'CPU core fact': cpucore_fact, 'RAM input': ram_input, 'RAM fact': ram_fact, 'Disk input': disk_input, 'Disk fact': disk_fact, 'Message': msg}
+                                output_dict = {'Hostname input': host_input, 'Hostname output': host_fact, 'Check-host': checkhost_fact, 'Port': port_fact, 'IP input': ipaddress_input, 'IP fact': ipaddress_fact, 'CPU core input': cpucore_input, 'CPU core fact': cpucore_fact, 'RAM input': ram_input, 'RAM fact': ram_fact, 'Disk input': disk_input, 'Disk fact': disk_fact, 'Message': msg}
                                 output_list.append(output_dict)
                                 fact_row_ctr += 1
-                            print('Finished processing fact file ' + fact_file)
                     else:
                         msg += "Fact file " + fact_file + " not found."
-                        output_dict = {'Hostname input': host_input, 'Hostname output': "", 'IP input': "", 'IP fact': "", 'CPU core input': "", 'CPU core fact': "", 'RAM input': "", 'RAM fact': "", 'Disk input': "", 'Disk fact': "", 'Message': msg}
+                        output_dict = {'Hostname input': host_input, 'Hostname output': "", 'Check-host': "", 'Port': "", 'IP input': "", 'IP fact': "", 'CPU core input': "", 'CPU core fact': "", 'RAM input': "", 'RAM fact': "", 'Disk input': "", 'Disk fact': "", 'Message': msg}
                         output_list.append(output_dict)
         ###print (time.strftime("%d/%m/%Y"))
         #output_file = "validation_" + time.strftime("%d%m%Y-%H%M") + ".csv"
         output_file = "output.csv"
         with open(output_file, 'w') as csvfile_output:      
-            fieldnames = ['Hostname input', 'Hostname output', 'IP input', 'IP fact', 'CPU core input', 'CPU core fact', 'RAM input', 'RAM fact', 'Disk input', 'Disk fact', 'Message']
+            fieldnames = ['Hostname input', 'Hostname output', 'Check-host', 'Port', 'IP input', 'IP fact', 'CPU core input', 'CPU core fact', 'RAM input', 'RAM fact', 'Disk input', 'Disk fact', 'Message']
             writer_output = csv.DictWriter(csvfile_output, fieldnames=fieldnames)
             writer_output.writeheader()
             writer_output.writerows(output_list)
