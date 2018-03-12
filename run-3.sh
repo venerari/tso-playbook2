fc5700028475e7fccbed0dccaf7913147cd781ce
Ricarte Venerayan <venerayan@gmail.com>
2018-03-11 23:50:41 -0400

    Update run-3.sh

--- run-3.sh
+++ run-3.sh
@@ -1,8 +1,13 @@
  
 #!/bin/bash
+
+#create hosts
+awk -F',' 'FNR > 1 { print  $1 }' input.csv > temp
+#remove empty lines
+awk NF temp > hosts
+
+chmod u+x read-csv-create2.py
  
- chmod u+x read-csv-create2.py
- 
- ./read-csv-create2.py input.csv
- cp server.yml roles/get-info-csv/vars/
- ansible-playbook -i hosts get-info-csv.yml
+./read-csv-create2.py input.csv
+cp server.yml roles/get-info-csv/vars/
+ansible-playbook -i hosts get-info-csv.yml
