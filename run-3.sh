0532d8f89619665eaf1a7a0b03313aaf2e9bb9c1
Ricarte Venerayan <venerayan@gmail.com>
2018-03-12 16:31:21 -0400

    Update run-3.sh

--- run-3.sh
+++ run-3.sh
@@ -5,9 +5,12 @@
 awk -F',' 'FNR > 1 { print  $1 }' input.csv > temp
 #remove empty lines
 awk NF temp > hosts
-
 chmod u+x read-csv-create2.py
- 
 ./read-csv-create2.py input.csv
 cp server.yml roles/get-info-csv/vars/
 ansible-playbook -i hosts get-info-csv.yml
+awk '{print "cp /tmp/fetched/"$1"/tmp/"$1"_output.csv ."}' hosts > cp-csv
+chmod u+x  cp-csv
+./cp-csv
+chmod u+x  validate.py
+./validate.py
